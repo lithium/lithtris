a559175d76400d136eaa1c0d6253146ccad0a16c
lithium <code@hlidskialf.com>
2013-09-22 12:28:52 -0700

    Set working directory to resource directory

--- Lithtris/SDLMain.m
+++ Lithtris/SDLMain.m
@@ -86,14 +86,8 @@ static NSString *getApplicationName(void)
 {
     if (shouldChdir)
     {
-        char parentdir[MAXPATHLEN];
-        CFURLRef url = CFBundleCopyBundleURL(CFBundleGetMainBundle());
-        CFURLRef url2 = CFURLCreateCopyDeletingLastPathComponent(0, url);
-        if (CFURLGetFileSystemRepresentation(url2, 1, (UInt8 *)parentdir, MAXPATHLEN)) {
-            chdir(parentdir);   /* chdir to the binary app's parent */
-        }
-        CFRelease(url);
-        CFRelease(url2);
+        NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+        [[NSFileManager defaultManager] changeCurrentDirectoryPath:resourcePath];
     }
 }
 
